ca7e87e7e82a41b8c1c3031b0d3daf2f50627c73
magaNasib <mnasibdev@gmail.com>
2023-12-29 18:00:26 +0400

    last

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,34 @@
 
+import { Box, Button, Flex, Heading, Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <Stack direction={'column'} p={'2'} borderRight={'1px'} borderColor={'gray.200'} minH={'100vh'}>
+            <Heading display='flex'  color={'red.500'}>Red<Text color={'green'}>Life</Text></Heading>
+            <Flex direction={'column'} p={'5'} gap={'3'} fontSize={'larger'}>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/'}>Messages</Link>
+                <Link to={'/'}>Search</Link>
+            </Flex>
+            <Menu>
+                <MenuButton as={Button} colorScheme='gray'>
+                    Profile
+                </MenuButton>
+                <MenuList>
+                    <MenuGroup title='Profile'>
+                        <MenuItem>My Account</MenuItem>
+                        <MenuItem>Payments </MenuItem>
+                    </MenuGroup>
+                    <MenuDivider />
+                    <MenuGroup title='Help'>
+                        <MenuItem>Docs</MenuItem>
+                        <MenuItem>FAQ</MenuItem>
+                    </MenuGroup>
+                </MenuList>
+            </Menu>
+        </Stack>
     )
 }
 
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
-
+import { Box,  Flex } from '@chakra-ui/react';
 
 export const Layout: React.FC = () => {
     return (
-        <>
-            <Header />
-            <Main>
-                <Outlet />
-            </Main>
-        </>
+        <Flex minH={'100vh'}>
+            <Box w={'20%'} p={'2'}>
+                <Header />
+            </Box>
+            <Box w={'80%'} p={'2'}>
+                <Main>
+                    <Outlet />
+                </Main>
+            </Box>
+        </Flex>
     );
 };
