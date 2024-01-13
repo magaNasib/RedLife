bed966f16d0db84d118f377cc57f69c60d73425b
RaisaYusifli <yusiflireise2003@gmail.com>
2024-01-13 19:57:08 +0400

    commentuiinput

--- src/features/HomeFeature/components/CardPost/index.tsx
+++ src/features/HomeFeature/components/CardPost/index.tsx
@@ -22,12 +22,12 @@ import {
 import { BiLike, BiChat, BiBookmark } from "react-icons/bi";
 import { FaArrowUp } from "react-icons/fa";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { IPost } from "../AddPost";
 import { Controller, useForm } from "react-hook-form";
 import { db } from "../../../../firebase";
-
+import CommentSection from "../Comments/CommentsSection";
 
 // interface IDonors {
 //   bloodGroup: string;
@@ -47,15 +47,7 @@ function CardPost() {
   const [loading, setLoading] = useState(true)
   const donorCollectionRef = collection(db, 'donors');
 
-
   const [showComment, setShowComment] = useState(false);
-
-  const methods = useForm<IPost>({
-    defaultValues: {
-      comment: ''
-    }
-  })
-
 
   useEffect(() => {
     const getPosts = async () => {
@@ -174,66 +166,11 @@ function CardPost() {
 
               </CardFooter>
               {showComment && (
-                <>
-                  <CardHeader>
-                    <Flex gap="4" mb={'40px'}>
-                      <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-                        <Avatar
-                          name={fullName}
-                          src={photoURL}
-                          borderColor="green.500"
-                          borderWidth="2px"
-                          bg={'black'}
-                        />
-                        <Box>
-                          <Heading size="sm">{fullName}</Heading>
-                          <Flex>
-                            <Text marginRight="2">{type}</Text>
-                            <Text>{bloodGroup}</Text>
-                          </Flex>
-                          <Flex alignSelf={'end'}>
-                            <Text marginRight="2">{city}</Text>
-                            <Text>{phone}</Text>
-                          </Flex>
-                          <Flex>
-                            <Text>{description}</Text>
-                          </Flex>
-                        </Box>
-                      </Flex>
-                      <IconButton
-                        variant="ghost"
-                        colorScheme="gray"
-                        aria-label="See menu"
-                        icon={<BsThreeDotsVertical />}
-                      />
-                    </Flex>
-                    <FormControl isInvalid={!!methods.formState.errors.comment}>
-                      <Controller
-                        control={methods.control}
-                        name='comment'
-                        rules={{
-                          required: 'This field is required'
-                        }}
-                        render={({ field }) => (
-                          <InputGroup>
-                            <Input {...field} value={field.value} onChange={field.onChange} onBlur={field.onBlur} type="text" placeholder="Add Comment" />
-                            <InputRightElement>
-                              <IconButton
-                                variant="ghost"
-                                colorScheme="blue"
-                                aria-label="See menu"><FaArrowUp />
-                              </IconButton>
-                            </InputRightElement>
-                          </InputGroup>
-                        )}
-                      />
-
-                      <FormErrorMessage>
-                        {methods.formState.errors?.comment?.message}
-                      </FormErrorMessage>
-                    </FormControl>
-                  </CardHeader>
-                </>
+                <CommentSection fullName={post.fullName} photoURL={post.photoURL} type={post.type} bloodGroup={post.bloodGroup} description={post.description} city={post.city} phone={post.phone} likes={[]} uid={""} id={post.id} publish_date={""} comments={{
+                  uid: "",
+                  message: "",
+                  date: ""
+                }} comment={""} />
               )}
             </Card>
           </Flex>
